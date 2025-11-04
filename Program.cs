6fc202d2748597494ec1f26c6ffae981feb62f5c
saleh salem <salehsalem.tech@gmail.com>
2025-11-04 19:30:11 +0200

    add identity for project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using ASPNETCoreIdentityDemo.Data;
 using ECom_wep_app.Models;
 using ECom_wep_app.Repository.Abstract;
 using ECom_wep_app.Repository.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +12,13 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EComDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 builder.Services.AddScoped<ICustomerRepoitory, CustomerRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -21,13 +28,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}`
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
